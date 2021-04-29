ca9901bb12749fa4abc12ca81570bfeaa808b0c1
brunoamaia <brunoamaia94@gmail.com>
2021-04-29 15:12:27 -0300

    create dataclass

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/BancoTeste.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Banco
+
+fun main () {
+    val digiOneBank = Banco(nome = "DigiOne", numero = 12)
+
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+    val bank2 = digiOneBank.copy(nome = "BancoDois")
+    println(bank2.info())
+
+}
