f089de1ef9c42def9a9a2a8bfe5bea3ed863b651
brunoamaia <brunoamaia94@gmail.com>
2021-04-29 14:52:00 -0300

    add secondary constructor and change properties to private

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,16 +3,24 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Bruno"
     var cpf: String = "123.456.789.10"
+    private set     // Não permite que os dados sejam alterados em outro escopo
 
     inner class Endereco {
         var rua: String = "Rua do caminho"
     }
+
+    constructor() // Criar um construir construtor secundário
+
+    fun mostrarDados() = "Nome: $nome e Cpf: $cpf"
 }
 
 fun main(){
-    val joao = Pessoa()
+    val bruno = Pessoa()
 
-    println(joao.nome)
-    println(joao.cpf)
-    println(joao.Endereco().rua)
+    println(bruno.nome)
+    println(bruno.cpf)
+    println(bruno.Endereco().rua)
+
+    println(bruno.mostrarDados())
+
 }
